ddb265e59dee63419e503811fe5c8913bbf32a95
svegagarrot <svegagarrot@itba.edu.ar>
2025-09-28 15:49:10 -0300

    SO initial commit

--- compilar.sh
+++ compilar.sh
@@ -1,6 +1,6 @@
-docker start ARQUI-TPE
-docker exec -it ARQUI-TPE make clean -C /root/Toolchain
-docker exec -it ARQUI-TPE make clean -C /root/
-docker exec -it ARQUI-TPE make -C /root/Toolchain
-docker exec -it ARQUI-TPE make -C /root/
-docker stop ARQUI-TPE
+docker start SO-TP02
+docker exec -it SO-TP02 make clean -C /root/Toolchain
+docker exec -it SO-TP02 make clean -C /root/
+docker exec -it SO-TP02 make -C /root/Toolchain
+docker exec -it SO-TP02 make -C /root/
+docker stop SO-TP02
