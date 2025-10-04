465190b16832123fc8b7444137e89e74eb55fd78
svegagarrot <svegagarrot@itba.edu.ar>
2025-10-04 18:05:07 -0300

    gdb listo

--- compilar.sh
+++ compilar.sh
@@ -4,3 +4,5 @@ docker exec -it SO-TP02 make clean -C /root/
 docker exec -it SO-TP02 make -C /root/Toolchain
 docker exec -it SO-TP02 make -C /root/
 docker stop SO-TP02
+
+# docker run -v ${PWD}:/root --security-opt seccomp=unconfined -ti agodio/itba-so-multi-platform:3.0
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #Flags para audio y la hora
-sudo qemu-system-x86_64 -machine type=pc,pcspk-audiodev=speaker -audiodev driver=coreaudio,id=speaker -hda Image/x64BareBonesImage.qcow2 -m 512 -rtc base=localtime
+if [[ "$1" = "gdb" ]]; then
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+else
+    sudo qemu-system-x86_64 -machine type=pc,pcspk-audiodev=speaker -audiodev driver=coreaudio,id=speaker -hda Image/x64BareBonesImage.qcow2 -m 512 -rtc base=localtime
+fi
